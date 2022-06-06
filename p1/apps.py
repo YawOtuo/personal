e349fde2d56783e3e146a72cf84f1fb1ae363a9d
YawOtuo <yotuo2003@gmail.com>
2022-06-06 15:44:21 +0000

    requiremnets.txt

---
+++ p1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class P1Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'p1'
---
+++ p1/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+
+from . import views
+
+app_name = 'p1'
+
+urlpatterns = [
+    path('',views.IndexView.as_view(), name='index'),
+]
